a826660e1a693cc6e06ad387078642bcc5ddd7fb
jpark163 <jpark163@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-10-25 21:03:45 +0000

    IDT working

---
+++ student-distrib/idt.h
@@ -0,0 +1,38 @@
+#ifndef _IDT_H
+#define _IDT_H
+
+#define 6_bit_mask 	0x1F
+#define 0_mask 		0
+#define 1_mask		1
+#define 0_dpl		0
+#define 3_dpl		3
+
+void set_trap_gate(uint32_t num, uint32_t address);
+void set_intr_gate(uint32_t num, uint32_t address);
+void set_system_gate(uint32_t num, uint32_t address);
+
+extern void init_idt();
+
+extern void divide_by_0();
+extern void debug();
+extern void non_mask_intr();
+extern void breakpoint();
+extern void overflow();
+extern void out_of_bounds();
+extern void invalid_op();
+extern void no_coprocessor();
+extern void double_fault();
+extern void coprocessor_segment_overrun();
+extern void bad_TSS();
+extern void no_segment();
+extern void stack_fault();
+extern void general_protection();
+extern void page_fault();
+extern void coprocessor_fault();
+extern void alignment_check();
+extern void machine_check();
+/* ask about last two lines */
+extern void simd_coprocessor_error();
+extern void ignore_int();
+
+#endif 
