d7c2f1933fcd227ce2047fd01bbac7ff35d47226
ryu12 <ryu12@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-10-26 22:04:16 +0000

    rtc files

---
+++ student-distrib/rtc.c
@@ -0,0 +1,14 @@
+#include "rtc.h"	
+	
+void rtc_init(){	
+	uint8_t prev_val;					// keep previous value of Register B
+	
+	outb(RTC_SEL_B, RTC_INDEX_PORT);	// Set index to select Register B
+	prev_val = inb(RTC_DATA_PORT);		// Read and save data
+	
+	outb(RTC_SEL_B, RTC_INDEX_PORT);	// Set index to Register B again
+	outb(prev_val | RTC_ENABLE_PIE, RTC_DATA_PORT);	// Enable Interrupt by enabling bit 6 of Reg B (PIE)
+	
+	enable_irq(IRQ2);					// turn on IRQ2
+	enable_irq(IRQ8);					// turn on IRQ8
+}
