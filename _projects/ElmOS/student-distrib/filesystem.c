63c869315527e2f9fc4b4013b14ff82d320cd953
jpark163 <jpark163@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-11-01 21:55:53 +0000

    data_read added to filesystem

--- _projects/ElmOS/student-distrib/filesystem.c
+++ _projects/ElmOS/student-distrib/filesystem.c
@@ -66,6 +66,74 @@ int32_t read_dentry_by_index (uint32_t index, dentry_t* dentry)
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length)
 {
-
+	uint32_t ret_val, remaining;
+	int num_block = offset/KB_4;
+	int block_index = offset % KB_4;
+	uint32_t *inode_ptr = BOOT_BLOCK_PTR + (inode+1)*KB_4;
+	uint32_t data_length = *inode_ptr;
+	uint32_t block_number = *(inode_ptr + ((num_block +1) *B_4));
+	uint8_t * block_ptr = (uint8_t *)(filesystem.data_start + ((block_number)*KB_4));
+	uint8_t * temp_buf = buf;
+	
+	// clears the buffer
+	memcpy(buf, 0, length);
+	
+	//checks if inode index is valid
+	if(inode < 0 || inode >= (filesystem.num_inodes - 1))
+	{
+		return -1;
+	}
+	//checks if offset is valid
+	if (offset >= data_length)
+	{
+		return -1;
+	}
+	
+	// This may or may not be right
+	if((offset+length) > data_length)
+	{
+		length = (data_length - offset);
+		ret_val = length;
+		remaining = length;
+	}
+	else
+	{
+		ret_val = length;
+		remaining = ret_val;
+	}
+		
+	//check if what's left is less than a block
+	if((block_index + remaining) <= KB_4)
+	{
+		memcpy(temp_buf,block_ptr,remaining);
+		remaining = 0;
+	}
+	else
+	{
+		memcpy(temp_buf, block_ptr, (KB_4 - block_index));
+		remaining -= KB_4 - block_index;
+		temp_buf += KB_4 - block_index;
+		num_block++;
+	}
+	
+	while(remaining > 0)
+	{
+		// check if what's remaining is greater than 1 block
+		if(remaining >= KB_4)
+		{
+			memcpy(temp_buf, block_ptr, KB_4);
+			remaining -= KB_4 - block_index;
+			temp_buf += KB_4 - block_index;
+			num_block++;
+		}
+		if(remaining < KB_4)
+		{
+			memcpy(temp_buf, block_ptr, remaining);
+			remaining = 0;
+		}
+	}
+	
+	return ret_val;
+	
 }
 
