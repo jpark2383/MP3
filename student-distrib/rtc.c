118c548f8ecac0bd53c533302ffd03142256fcfa
ryu12 <ryu12@f4af8d5a-4da7-4976-bb6f-9ed95603f7bd>
2014-11-02 04:02:55 +0000

    rtc written

--- student-distrib/rtc.c
+++ student-distrib/rtc.c
@@ -1,8 +1,9 @@
 /* rtc.c - Functions to interact with the RTC
  * vim:ts=4 noexpandtab
  */
-
 #include "rtc.h"	
+
+volatile int32_t intr_flag = 0;
 
 /*
 * rtc_init
@@ -10,27 +11,135 @@
 *   INPUTS: None
 *   OUTPUTS: None
 *   RETURN VALUE: None
-*   SIDE EFFECTS: Initilizes the rtc to frequency 2Hz
+*   SIDE EFFECTS: Initializes the RTC to frequency 2Hz
 */ 
 void rtc_init(){	
 	uint8_t prev_val;					// keep previous value of Register B
 	
+	cli();
 	outb(RTC_SEL_B, RTC_INDEX_PORT);	// Set index to select Register B
 	prev_val = inb(RTC_DATA_PORT);		// Read and save data
-	
 	outb(RTC_SEL_B, RTC_INDEX_PORT);	// Set index to Register B again
 	outb(prev_val | RTC_ENABLE_PIE, RTC_DATA_PORT);	// Enable Interrupt by enabling bit 6 of Reg B (PIE)
-	
-	cli();
-	int rate = 15;
-	outb(0x8A, 0x70);		// set index to register A, disable NMI
-	char prev = inb(0x71);	// get initial value of register A
-	outb(0x8A, 0x70);		// reset index to A
-	outb((prev & 0xF0) | rate, 0x71); //write only our rate to A. Note, rate is the bottom 4 bits.
+		
+	outb(RTC_SEL_A, RTC_INDEX_PORT);		// set index to register A, disable NMI
+	prev_val = inb(RTC_DATA_PORT);			// get initial value of register A
+	outb(RTC_SEL_A, RTC_INDEX_PORT);		// reset index to A
+	outb((prev_val & RTC_WRITE_MASK) | RTC_BASE_FREQ, RTC_DATA_PORT); //write only our rate to A. Note, rate is the bottom 4 bits.
 	sti();
+	
 	enable_irq(IRQ2);					// turn on IRQ2
 	enable_irq(IRQ8);					// turn on IRQ8
 }
+
+/*
+* rtc_open
+*   DESCRIPTION: open RTC
+*   INPUTS: None
+*   OUTPUTS: None
+*   RETURN VALUE: None
+*   SIDE EFFECTS: None
+*/ 
+int32_t rtc_open(const uint8_t* filename){
+	rtc_init();
+	return 0;
+}
+
+/*
+* rtc_close
+*   DESCRIPTION: close RTC
+*   INPUTS: None
+*   OUTPUTS: None
+*   RETURN VALUE: None
+*   SIDE EFFECTS: None
+*/ 
+int32_t rtc_close(int32_t fd){
+	return 0;
+}
+
+/*
+* rtc_read
+*   DESCRIPTION: 
+*   INPUTS: None
+*   OUTPUTS: None
+*   RETURN VALUE: None
+*   SIDE EFFECTS: None
+*/ 
+int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes){
+	//printf("entering RTC_read... \n");
+	intr_flag = 0;
+	while(intr_flag == 0);
+	
+	//printf("exitting RTC_read... \n");
+	return 0;
+}
+
+/*
+* rtc_write
+*   DESCRIPTION: 
+*   INPUTS: fd - file descriptor
+*			buf - void pointer to Hz to write
+*			nbytes - number of bytes to write
+*   OUTPUTS: None
+*   RETURN VALUE: None
+*   SIDE EFFECTS: None
+*/ 
+int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes){
+
+	if (((const int32_t*)buf) == NULL)
+		return -1;
+	else if (nbytes != 4)
+		return -1;
+		
+	int32_t intr_freq = *((const int32_t*)buf);
+	int32_t po2_flag = 0;
+	int32_t i;
+	
+	for (i = 2; i <= 1024; i *= 2){
+		if (intr_freq == i)
+			po2_flag = 1;
+	}
+	
+	if (!po2_flag)
+		return -1;
+	else 
+		rtc_set(intr_freq);
+	
+	return 0;
+}
+
+/*
+* rtc_set
+*   DESCRIPTION: helper function to set the RTC interrupt frequency
+*   INPUTS: None
+*   OUTPUTS: None
+*   RETURN VALUE: None
+*   SIDE EFFECTS:
+*/ 
+void rtc_set(int32_t freq)
+{
+	int32_t i;
+	uint8_t prev_val;
+	uint8_t write_freq = RTC_BASE_FREQ;
+	
+	if (freq < RTC_MIN)
+		freq = RTC_MIN;
+	else if ( freq > RTC_MAX)
+		freq = RTC_MAX;
+	
+	for(i = 2; i < freq ; i *= 2){
+		write_freq--;
+	}
+	
+	cli();
+	outb(RTC_SEL_A, RTC_INDEX_PORT);
+	prev_val = inb(RTC_DATA_PORT);	
+	outb(RTC_SEL_A, RTC_INDEX_PORT);
+	outb((prev_val & RTC_WRITE_MASK) | write_freq, RTC_DATA_PORT);
+	sti();	
+
+}
+
 
 /*
 * rtc_interrupt
@@ -38,15 +147,17 @@ void rtc_init(){
 *   INPUTS: None
 *   OUTPUTS: None
 *   RETURN VALUE: None
-*   SIDE EFFECTS: Initilizes the rtc to frequency 2Hz
+*   SIDE EFFECTS: Initializes the rtc to frequency 2Hz
 */ 
 void rtc_interrupt()
 {
-	test_interrupts(); // Run test_interrupts
-	//putc('s');
-	outb(0x0C, 0x70);	// select register C
-	inb(0x71);		// throw away data contents
+	//test_interrupts(); 					// Run test_interrupts
+	outb(RTC_SEL_C, RTC_INDEX_PORT);	// select register C
+	inb(RTC_DATA_PORT);					// throw away data contents
 
-	send_eoi(8); //Send EOIs
-	send_eoi(2);
+	intr_flag++;						// clear 
+	//printf("intr_flag is: %d \n", intr_flag);
+	
+	send_eoi(IRQ8); 					//Send EOIs
+	send_eoi(IRQ2);
 }
--- student-distrib/rtc.h
+++ student-distrib/rtc.h
@@ -16,10 +16,21 @@
 #define RTC_ENABLE_PIE 0x40
 #define RTC_INDEX_PORT 0x70
 #define RTC_DATA_PORT 0x71
+#define RTC_BASE_FREQ 0x0F
+#define RTC_WRITE_MASK 0xF0
+#define RTC_MIN 2
+#define RTC_MAX 1024
 #define IRQ2 0x02
 #define IRQ8 0x08
 
+
+
 extern void rtc_init(); // Initilization of the RTC. Starts at 2
 extern void rtc_interrupt(); // Handler for RTC interrupts
+extern int32_t rtc_open(const uint8_t* filename);
+extern int32_t rtc_close(int32_t fd);
+extern int32_t rtc_read(int32_t fd, void* buf, int32_t nbytes);
+extern int32_t rtc_write(int32_t fd, const void* buf, int32_t nbytes);
+extern void rtc_set(int32_t freq); // helper for setting RTC frequency
 
 #endif
